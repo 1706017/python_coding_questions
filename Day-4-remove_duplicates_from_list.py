1fbd4b51de06c427910960041640e089377a24c0
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2024-10-23 23:07:59 +0530

    Day-4-remove_duplicates_from_list.py

---
+++ Day-4-remove_duplicates_from_list.py
@@ -0,0 +1,16 @@
+
+my_lst = [12,12,67,89,90,90,34] 
+
+my_new_lst = set(my_lst)
+
+print("New List after removal of duplicates are as follows")
+
+for i in list(my_new_lst):
+    print(i)
+
+
+
+    
+    
+
+
